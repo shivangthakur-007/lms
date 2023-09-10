2b32ac21fa9776063c90adcdfc169ca816ceb76d
Dance <thakurshivang579@gmail.com>
2023-09-10 09:47:52 +0530

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Footer from './Components/Footer'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <Footer />
     </>
   )
 }
---
+++ src/assets/Helper/axiosInstance.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+const Base_Url= 'http://localhost:5014/api/v1';
+
+const axiosInstance= axios.create();
+
+axiosInstance.defaults.baseURL= Base_Url;
+axiosInstance.defaults.withCredentials=true;
